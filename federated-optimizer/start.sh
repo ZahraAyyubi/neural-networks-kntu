e4da9490ce18282be34089fea6602caf9d68f907
ZahraAyyubi <45013674+ZahraAyyubi@users.noreply.github.com>
2025-05-09 19:57:54 +0330

    add files

---
+++ federated-optimizer/start.sh
@@ -0,0 +1,18 @@
+# Start the server
+python server.py &
+
+# Wait for the server to start
+sleep 1
+
+# Start 10 client processes
+python client1.py &
+python client2.py &
+python client3.py &
+python client4.py &
+python client5.py &
+python client6.py &
+python client7.py &
+python client8.py &
+python client9.py &
+python client10.py
+
